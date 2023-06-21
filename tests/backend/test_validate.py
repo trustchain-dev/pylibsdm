e22fd32109c70eab63f45a880d6859859627fd9b
Dominik George <nik@naturalnet.de>
2023-06-21 18:13:42 +0200

    Add doc references in tests

--- tests/backend/test_validate.py
+++ tests/backend/test_validate.py
@@ -1,9 +1,14 @@
+"""Test cases derived from examples in NXP's documentation.
+
+Reference: https://www.nxp.com/docs/en/application-note/AN12196.pdf
+"""
 from binascii import unhexlify
 
 from pylibsdm.backend.validate import ParamValidator
 
 
 def test_generate_sdm_session_keys():
+    # ref: page 10, table 2
     validator = ParamValidator(unhexlify("5ACE7E50AB65D5D51FD5BF5A16B8205B"))
     validator.uid = unhexlify("04C767F2066180")
     validator.read_ctr = 1
@@ -15,6 +20,7 @@ def test_generate_sdm_session_keys():
 
 
 def test_decrypt_picc_data():
+    # ref: page 12, table 3
     validator = ParamValidator()
     validator.decrypt_picc_data("EF963FF7828658A599F3041510671E88")
 
@@ -23,6 +29,7 @@ def test_decrypt_picc_data():
 
 
 def test_decrypt_file_data():
+    # ref: page 13, table 4
     validator = ParamValidator()
     file_data = validator.decrypt_file_data(
         "94592FDE69FA06E8E3B6CA686A22842B", "FDE4AFA99B5C820A2C1BB0F1C792D0EB"
@@ -32,12 +39,14 @@ def test_decrypt_file_data():
 
 
 def test_validate_cmac_zero_length():
+    # ref: apge 15, table 5
     validator = ParamValidator()
 
     assert validator.validate_cmac("94EED9EE65337086", "EF963FF7828658A599F3041510671E88")
 
 
 def test_validate_cmac_input():
+    # ref: page 17, table 6
     validator = ParamValidator()
 
     assert validator.validate_cmac(
